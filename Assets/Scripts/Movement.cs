27c413608568a6deb0a47d863ceabf9cce271ed1
Pedro <pgarbozahong3@gatech.edu>
2025-02-06 00:15:15 -0500

    Adding player and movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+
+    private Rigidbody rigidBody;
+
+    void Start()
+    {
+
+        rigidBody = GetComponent<Rigidbody>();
+
+    }
+    void FixedUpdate()
+    {
+
+        float vertical = Input.GetAxis("Vertical");
+
+        float horizontal = Input.GetAxis("Horizontal");
+        
+        Vector3 movement = new Vector3(horizontal, 0, vertical) * speed;
+
+        rigidBody.MovePosition(rigidBody.position + movement * Time.fixedDeltaTime);
+    }
+}
