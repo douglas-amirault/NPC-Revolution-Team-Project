6e0c5521c24ea364f4d26a7a139b592c5140e94b
Madeoy Gault, Faith Elizabeth <fgault3@gatech.edu>
2025-02-08 12:40:41 -0500

    simple player cam

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    // toggle for debug purposes
+    public Camera overhead;
+    private Camera playerCam;
+    private bool overheadOn;
+
+    public GameObject player;
+    private Vector3 offset;
+
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        overheadOn = true;
+
+        overhead = Camera.main;
+        playerCam = GetComponent<Camera>();
+        playerCam.enabled = false;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        playerCam.transform.position = player.transform.position + offset;
+        
+        // bleh can't get rotiation working
+        //playerCam.transform.rotation = player.transform.rotation;
+    }
+
+    private void Update()
+    {
+        // switch camera on button toggle
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            // switch to player
+            if(overheadOn)
+            {
+                overhead.enabled = false;
+                playerCam.enabled = true;
+                overheadOn = false;
+            }
+
+            // switch to overhead
+            else
+            {
+                overhead.enabled = true;
+                playerCam.enabled = false;
+                overheadOn = true;
+            }
+        }
+    }
+
+
+}
