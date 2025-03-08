6d6450e77b2c293526218416e40da35e9019938d
Derek Griffing <dgriffing3@gatech.edu>
2025-03-08 17:01:00 -0500

    Make it charge faster so it attacks on time

--- Assets/Scripts/ChasingRoomba.cs
+++ Assets/Scripts/ChasingRoomba.cs
@@ -9,7 +9,10 @@ using UnityEngine.AI;
 // subset of roomba: won't give warning but no waypoints and has extra detection for 
 public class ChasingRoomba : Roomba
 {
-
+    // The Roombas were charging too slow when relying on animation to update
+    // clip name; added these var to speed charging
+    private float chargeDelay = 0.2f;
+    private float chargeTimer = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,15 +36,33 @@ public class ChasingRoomba : Roomba
                         Time.deltaTime * 2);
 
                 // identify when roomba is ready by if it got past pre-charge animation
-                string clipName = anim.GetCurrentAnimatorClipInfo(0)[0].clip.ToString();
-                if( clipName.StartsWith("ChasingRoombaRun"))
+                // string clipName = anim.GetCurrentAnimatorClipInfo(0)[0].clip.ToString();
+                // if( clipName.StartsWith("ChasingRoombaRun"))
+                // {
+                //     roombaState = RoombaState.Charge;
+                // }
+                // DG: relying on teh animator to change clip name may have led
+                // to slow charging - change below is consistent with lectures
+                // where we use delta time to make things indpendent of frame
+                // rate. This seems to speed charging and improve gameplay.
+                chargeTimer += Time.deltaTime;
+                if (chargeTimer >= chargeDelay)
                 {
                     roombaState = RoombaState.Charge;
+                    chargeTimer = 0f;
                 }
             }
         }
         else if (roombaState == RoombaState.Charge)
         {
+            // DG: Added this because without it the Roomba locked its rotation
+            // on player and missed. Here we continue to update rotation.
+            if (player != null)
+            {
+                transform.rotation = Quaternion.LerpUnclamped(transform.rotation,
+                    Quaternion.LookRotation((player.position - transform.position), Vector3.up),
+                    Time.deltaTime * 2);
+            }
             rbody.AddForce( transform.forward * roombaSpeed, ForceMode.Impulse);
         }
     }
